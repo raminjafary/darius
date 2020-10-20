a9d38c72547fb13fc884fbc69ce579d9fd86294f
ramin <raminjafary1993@gmail.com>
2020-10-21 01:07:46 +0330

    feat: add ANSI escape code sample

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 mod parse;
-use parse::{parse_args};
+use parse::parse_args;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // It is just an example and should be deleted!
-    parse_args(args)
+    parse_args(args);
 }
